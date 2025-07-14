4582e0a8d96a63af8d91ada1e6cea15d06624616
Muadz Yamani <muadzyamani@gmail.com>
2025-07-14 09:15:12 +0800

    Refactor Main class to include color variables and print statements for trunk and brighter colors

--- Exercise Files/01_05/begin/Objects/src/Main.java
+++ Exercise Files/01_05/begin/Objects/src/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         Color myTrunkColor = Tree.TRUNK_COLOR;
+        Color myDefaultWhite = Color.WHITE;
+        Color myDefaultBlue = Color.BLUE;
+        Color myDefaultDarkGray = Color.DARK_GRAY;
+
+        Color brighterBlue = myDefaultBlue.brighter();
+        Color brighterDarkGray = myDefaultDarkGray.brighter();
+
+        System.out.println(myDefaultDarkGray);
+        System.out.println(brighterDarkGray);
     }
 }
