ddf9a5989820c6993efe6043ccd173599ce293e2
Muadz Yamani <muadzyamani@gmail.com>
2025-07-16 10:31:11 +0800

    Update 03_03

--- Exercise Files/03_03/start/Inheritance/src/Analyst.java
+++ Exercise Files/03_03/start/Inheritance/src/Analyst.java
@@ -1,32 +1,10 @@
-public class Analyst {
-
-    private String name;
-    private double salary;
-    private int age;
+public class Analyst extends Employee {
 
     public Analyst(String name, double salary, int age) {
-        this.name = name;
-        this.salary = salary;
-        this.age = age;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public double getSalary() {
-        return this.salary;
+        super(name, salary, age);
     }
 
     public double getAnnualBonus() {
         return this.salary * .05;
-    }
-
-    public void raiseSalary() {
-        this.salary = this.salary * 1.2;
     }
 }
--- Exercise Files/03_03/start/Inheritance/src/Salesperson.java
+++ Exercise Files/03_03/start/Inheritance/src/Salesperson.java
@@ -1,36 +1,15 @@
-public class Salesperson {
+public class Salesperson extends Employee {
 
-    private String name;
-    private double salary;
-    private int age;
     private double commissionPercentage;
 
     public Salesperson(String name, double salary, int age,
                        double commissionPercentage) {
-        this.name = name;
-        this.salary = salary;
-        this.age = age;
+        super(name, salary, age);
         this.commissionPercentage = commissionPercentage;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public double getSalary() {
-        return this.salary;
-    }
-
-    public int getAge() {
-        return this.age;
     }
 
     public double getCommissionPercentage() {
         return this.commissionPercentage;
-    }
-
-    public void raiseSalary() {
-        this.salary = this.salary * 1.2;
     }
 
     public void raiseCommission() {
