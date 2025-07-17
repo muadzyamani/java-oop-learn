dadc28dbe8d65afff67c813f6fa182d43583eb89
Muadz Yamani <muadzyamani@gmail.com>
2025-07-17 10:01:17 +0800

    Update 04_04

--- Exercise Files/04_05/start/Polymorphism/src/ConditionArrayList.java
+++ Exercise Files/04_05/start/Polymorphism/src/ConditionArrayList.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
@@ -11,7 +12,12 @@ public class ConditionArrayList extends ArrayList<Integer> {
 
     public ConditionArrayList(Predicate<Integer> predicate,
                               Integer... nums) {
-        super(Arrays.stream(nums)
+        super(new ConditionArrayList(predicate, Arrays.asList(nums)));
+    }
+
+    public ConditionArrayList(Predicate<Integer> predicate,
+                              List<Integer> arrayList) {
+        super(arrayList.stream()
                 .filter(predicate)
                 .collect(Collectors.toList()));
         this.condition = predicate;
