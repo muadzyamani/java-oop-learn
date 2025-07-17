acd14b65f1abd0abd4045dcbad8075ee4af8ce1c
Muadz Yamani <muadzyamani@gmail.com>
2025-07-17 09:49:18 +0800

    Update 04_04

--- Exercise Files/04_04/start/Polymorphism/src/ConditionArrayList.java
+++ Exercise Files/04_04/start/Polymorphism/src/ConditionArrayList.java
@@ -1,27 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
-public class OddArrayList extends ArrayList<Integer> {
+public class ConditionArrayList extends ArrayList<Integer> {
 
-    public OddArrayList(Integer... nums) {
+    private Predicate<Integer> condition;
+
+    public ConditionArrayList(Predicate<Integer> predicate,Integer... nums) {
         super(Arrays.stream(nums)
-                .filter(OddArrayList::isOdd)
+                .filter(predicate)
                 .collect(Collectors.toList()));
+        this.condition = predicate;
     }
 
     @Override
     public void add(int index, Integer element) {
-        if (isOdd(element)) {
+        if (isEligible(element)) {
             super.add(index, element);
         }
     }
 
     @Override
     public boolean add(Integer element) {
-        if (isOdd(element)) {
+        if (isEligible(element)) {
             return super.add(element);
         } else {
             return false;
@@ -31,7 +35,7 @@ public class OddArrayList extends ArrayList<Integer> {
     @Override
     public boolean addAll(Collection<? extends Integer> c) {
         return super.addAll(c.stream()
-                .filter(OddArrayList::isOdd)
+                .filter(this::isEligible)
                 .collect(Collectors.toList()));
     }
 
@@ -39,14 +43,14 @@ public class OddArrayList extends ArrayList<Integer> {
     public boolean addAll(int index,
                           Collection<? extends Integer> c) {
         return super.addAll(index, c.stream()
-                .filter(OddArrayList::isOdd)
+                .filter(this::isEligible)
                 .collect(Collectors.toList()));
     }
 
     @Override
     public Integer set(int index,
                        Integer element) {
-        if (isOdd(element)) {
+        if (isEligible(element)) {
             return super.set(index, element);
         } else {
             System.out.println(element + " is not odd. ");
@@ -58,11 +62,11 @@ public class OddArrayList extends ArrayList<Integer> {
     @Override
     public void replaceAll(UnaryOperator<Integer> operator) {
         super.replaceAll(operator);
-        super.removeIf(n -> !isOdd(n));
+        super.removeIf(n -> !isEligible(n));
     }
 
-    public static boolean isOdd(Integer element) {
-        return Math.abs(element) % 2 == 1;
+    public boolean isEligible(Integer element) {
+        return condition.test(element);
     }
 
 }
